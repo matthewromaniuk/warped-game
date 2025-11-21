1c270110b29aacb3be28537232622f8d18594ebc
Carlton IV <carltonchampion1@gmail.com>
2025-11-20 23:01:49 -0600

    COMMIT

--- warped-game/gui.gd
+++ warped-game/gui.gd
@@ -1,17 +1,20 @@
 extends CanvasLayer
 
+@onready var timer = $Timer
+
 func _process(_delta: float) -> void:
 	if !$Timer.is_stopped():
-		if $Timer.time_left < 10:
+		if $Timer.time_left < 3:
 			$Timer/Time.modulate = Color(1,0,0)
 			$Timer/Time.text = str($Timer.time_left).substr(0,4)
-		elif $Timer.time_left <= 30:
+		elif $Timer.time_left <= 6:
 			$Timer/Time.modulate = Color(1,.6,0)
 			$Timer/Time.text = str($Timer.time_left).substr(0,4)
 		else:
-			$Timer/Time.modulate = Color(1,1,1)
+			$Timer/Time.modulate = Color(0,1,0)
 			$Timer/Time.text = str(int(floor($Timer.time_left)))
 	elif !$Timer.time_left:
 		$Timer/Time.modulate = Color(1,0,0)
 		$Timer/Time.text = "0:00"
+		global.failed = true
 	else: $Timer/Time.text = ""
--- warped-game/portal_script.gd
+++ warped-game/portal_script.gd
@@ -1,6 +1,5 @@
 extends Node
 
-var cooldown := 0
 var player1_portal 
 var player2_portal
 # Called when the node enters the scene tree for the first time.
@@ -10,20 +9,21 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if cooldown:
-		cooldown -= 1
+	pass
 
 func check_portals() -> void:
 	if !get_parent().get_node("player1").portal:
 		pass
-	elif get_parent().get_node("player1").portal == get_parent().get_node("player2").portal and !cooldown:
-		var temp = get_parent().get_node("player1").position
+	elif get_parent().get_node("player1").portal == get_parent().get_node("player2").portal:
 		get_parent().get_node("player1").portal_animation()
 		get_parent().get_node("player2").portal_animation()
 		await get_tree().create_timer(1).timeout
-		get_parent().get_node("player1").position = get_parent().get_node("player2").position
-		get_parent().get_node("player2").position = temp
-		cooldown = 60
+		if !global.failed:
+			global.level += 1
+			get_tree().change_scene_to_file("res://level"+str(global.level)+".tscn")
+		else:
+			get_tree().change_scene_to_file("res://level1.tscn")
+			global.failed = false
 
 func player_in_portal() -> bool:
 	var player_present = false
@@ -45,14 +45,4 @@ func light_portals(player : int ,portal : String):
 
 func unlight_portals(player : int):
 	if !player_in_portal():
-		var portal
-		if player == 1:
-			portal = player1_portal
-		else:
-			portal = player2_portal
-		var tween = get_tree().create_tween()
-		tween.set_parallel(true)
-		get_parent().get_node("Portal " + portal + " 1").get_child(0).stop()
-		tween.tween_property(get_parent().get_node("Portal " + portal + " 1"),"modulate", Color(1,1,1), 0.2)
-		get_parent().get_node("Portal " + portal + " 2").get_child(0).stop()
-		tween.tween_property(get_parent().get_node("Portal " + portal + " 2"),"modulate", Color(1,1,1), 0.2)
+		print("you shouldnt be reading thiss")
